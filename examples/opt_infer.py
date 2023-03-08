9d7255250c6ff2a59e1304bca6779cfa3a1037f8
JunXiong <jun.xiong@lightelligence.ai>
2023-03-08 14:59:55 +0800

    adding opt_infer.py

--- examples/opt_infer.py
+++ examples/opt_infer.py
@@ -7,14 +7,52 @@ from transformers import pipeline
 from transformers import AutoTokenizer
 
 
+local_rank = int(os.getenv("LOCAL_RANK", "0"))
+world_size = int(os.getenv("WORLD_SIZE", "1"))
+
+deepspeed.init_distributed("nccl")
+rank = dist.get_rank()
+
+
+def print_rank0(*msg):
+    if rank != 0:
+        return
+    print(*msg)
+
 
 def main(args):
     model = load_model(args.model_path)
+    model.eval()
     tokenizer = AutoTokenizer.from_pretrained("facebook/opt-66b",
                                               padding_side="left")
     generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
+    model_hidden_size = model.config.hidden_size
+    train_bs = 8
+    ds_config = {
+        "fp16": {
+            "enabled": True,
+        },
+        "bf16": {
+            "enabled": False,
+        },
+        "zero_optimization": {
+            "stage": 3,
+            "overlap_comm": True,
+            "contiguous_gradients": True,
+            "reduce_bucket_size": model_hidden_size * model_hidden_size,
+            "stage3_prefetch_bucket_size": 0.9 * model_hidden_size * model_hidden_size,
+            "stage3_param_persistence_threshold": 0,
+        },
+        "steps_per_print": 2000,
+        "train_batch_size": train_bs,
+        "train_micro_batch_size_per_gpu": 1,
+        "wall_clock_breakdown": False,
+    }
+
+    ds_config["zero_optimization"]["offload_param"] = dict(device="cpu", pin_memory=True)
+
     generator.model = deepspeed.init_inference(generator.model,
-                                               config=args.conf_path,
+                                               config=ds_config,
                                                checkpoint=None,
                                                replace_with_kernel_inject=True)
     output = generator('DeepSpeed is', do_sample=True, min_length=10)
